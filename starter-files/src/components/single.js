326477bc43decb277f268ca5fa06cecfe2189124
Gabriel Halley <gabhalley@gmail.com>
2016-10-19 15:35:52 -0500

    first crack at second exercise

--- starter-files/src/components/single.js
+++ starter-files/src/components/single.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import Header from './header'
+import Loader from './loader'
 
 const Single = React.createClass({
+  getInitialState() {
+    return {
+      beer: {},
+      loading: true
+    }
+  },
+
+  loadBeer() {
+    this.setState({ loading: true });
+    const beerID = this.props.params.beerId
+    fetch(`http://api.react.beer/v2/beer/${beerID}`)
+    .then(data => data.json())
+    .then((beer) => {
+      this.setState({
+        beer: beer.data,
+        loading: false
+      });
+    })
+    .catch(err => console.error(err));
+  },
+
+  componentWillMount() {
+    this.loadBeer();
+  },
+
   render() {
+    if (this.state.loading) {
+      return <Loader message="Pouring a cold one!"/>
+    }
     return (
-      <div className="single-beer">Single beer will go here</div>
+      <div>
+        <Header siteName="Beer Me" />
+        <div className="single-beer">
+          { this.state.beer['name'] }
+          <br/>
+          { this.state.beer['description'] }
+        </div>
+      </div>
     )
   }
 });
