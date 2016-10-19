df51ff125fc87cde754e5f7fa85d1ef6b5707783
Gabriel Halley <gabhalley@gmail.com>
2016-10-19 11:32:36 -0500

    props

--- starter-files/src/components/header.js
+++ starter-files/src/components/header.js
@@ -5,10 +5,15 @@ import { Link } from 'react-router';
 const Header = React.createClass({
   // Link tag uses push state to change url of browser without reloading whole page.
   // React responds to the change in the url alone
+
+  propTypes: {
+    siteName: React.PropTypes.string.isRequired //if not required, leave this method off
+  },
+
   render() {
     return (
       <h1>
-        <Link to="/">Beer Me!</Link>
+        <Link to="/">{this.props.siteName}</Link>
       </h1>
     )
   }
