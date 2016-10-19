f3b4285ee295a2c96ef6612bb4e36bdfc4b268f7
Gabriel Halley <gabhalley@gmail.com>
2016-10-19 11:16:03 -0500

    second react component

--- starter-files/src/index.js
+++ starter-files/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom'; //can import pieces of modules like this
-import Header from './components/header'
+import Main from './components/main'
 
 import './style.css';
 
-render(<Header />, document.querySelector('#root'));
+render(<Main />, document.querySelector('#root'));
 
